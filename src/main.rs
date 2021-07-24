ed08b3f8930c1c301a7dd15cdecb22faab352415
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-07-23 18:19:36 -0600

    Added parallel processing with Rayon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use image::{GrayImage, ImageBuffer, Luma};
 use num::complex::Complex;
+use rayon::prelude::*;
 use std::convert::TryInto;
 
 fn calculate_mandelbrot(
@@ -10,31 +11,27 @@ fn calculate_mandelbrot(
     y_max: f64,
     width: usize,
     height: usize,
-) -> Vec<Vec<usize>> {
-    let mut rows: Vec<_> = Vec::with_capacity(width);
-    for img_y in 0..height {
-        let mut row: Vec<usize> = Vec::with_capacity(height);
+) -> Vec<usize> {
+    let mut pixels = vec![0; width * height];
+    let rows: Vec<(usize, &mut [usize])> = pixels.chunks_mut(width).enumerate().collect();
+    rows.into_par_iter().for_each(|(img_y, row)| {
+        let y_percent = img_y as f64 / height as f64;
         for img_x in 0..width {
             let x_percent = img_x as f64 / width as f64;
-            let y_percent = img_y as f64 / height as f64;
             let cx = x_min + (x_max - x_min) * x_percent;
             let cy = y_min + (y_max - y_min) * y_percent;
             let escaped_at = mandelbrot_at_point(cx, cy, max_iters);
-            //println!("{} | {}", Complex::new(cx, cy), escaped_at);
-            row.push(escaped_at);
+            row[img_x] = escaped_at;
         }
-        rows.push(row);
-    }
-    rows
+    });
+    pixels
 }
 
 fn mandelbrot_at_point(cx: f64, cy: f64, max_iters: usize) -> usize {
-    //println!("{}, {}", cx, cy);
     let mut z = Complex { re: 0.0, im: 0.0 };
     let c = Complex::new(cx, cy);
 
     for i in 0..=max_iters {
-        //println!("{}", z.norm());
         if z.norm() > 2.0 {
             return i;
         }
@@ -43,17 +40,15 @@ fn mandelbrot_at_point(cx: f64, cy: f64, max_iters: usize) -> usize {
     max_iters
 }
 
-fn render_mandelbrot(escape_vals: Vec<Vec<usize>>, max_iters: usize, width: u32, height: u32) {
+fn render_mandelbrot(pixels: Vec<usize>, max_iters: usize, width: u32, height: u32) {
     let mut img: GrayImage = ImageBuffer::new(width, height);
-    let mut y = 0;
-    for row in escape_vals {
-        let mut x = 0;
-        for column in row {
-            let val: u8 = (column * 255 / max_iters).try_into().unwrap();
-            img.put_pixel(x, y, Luma([val]));
-            x += 1;
-        }
-        y += 1;
+    let mut i = 0;
+    for pixel in pixels {
+        let y = i / width;
+        let x = i - (y * width);
+        let val: u8 = (pixel * 255 / max_iters).try_into().unwrap();
+        img.put_pixel(x, y, Luma([val]));
+        i += 1;
     }
     img.save(format!("mb_{}_{}x{}.png", max_iters, width, height))
         .unwrap();
